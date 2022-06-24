a763dc532553736ff817093ff087fe1e14e94444
Dimitri Capetz <dimitri.capetz@gmail.com>
2022-06-24 18:31:39 +0000

    Updated MAC script

--- containerlab-topos/rc-campus-topo/mac.sh
+++ containerlab-topos/rc-campus-topo/mac.sh
@@ -1,56 +1,28 @@
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:10:00:00/' clab-rc-campus-topo/CAMPUS-SPINE1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:11:00:00/' clab-rc-campus-topo/CAMPUS-SPINE2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:18:00:00/' clab-rc-campus-topo/CAMPUS-FL1-C1-SPLINE1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:19:00:00/' clab-rc-campus-topo/CAMPUS-FL1-C1-SPLINE2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:20:00:00/' clab-rc-campus-topo/CAMPUS-FL1-C2-SPLINE1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:21:00:00/' clab-rc-campus-topo/CAMPUS-FL1-C2-SPLINE2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:22:00:00/' clab-rc-campus-topo/CAMPUS-FL2-C1-SPLINE1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:23:00:00/' clab-rc-campus-topo/CAMPUS-FL2-C1-SPLINE2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:24:00:00/' clab-rc-campus-topo/CAMPUS-FL2-C2-SPLINE1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:25:00:00/' clab-rc-campus-topo/CAMPUS-FL2-C2-SPLINE2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:26:00:00/' clab-rc-campus-topo/CAMPUS-FL3-C1-SPLINE1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:27:00:00/' clab-rc-campus-topo/CAMPUS-FL3-C1-SPLINE2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:28:00:00/' clab-rc-campus-topo/CAMPUS-FL3-C2-SPLINE1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:29:00:00/' clab-rc-campus-topo/CAMPUS-FL3-C2-SPLINE2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:30:00:00/' clab-rc-campus-topo/CAMPUS-FL1-C1-LEAF1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:31:00:00/' clab-rc-campus-topo/CAMPUS-FL1-C1-LEAF2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:32:00:00/' clab-rc-campus-topo/CAMPUS-FL1-C2-LEAF1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:33:00:00/' clab-rc-campus-topo/CAMPUS-FL1-C2-LEAF2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:34:00:00/' clab-rc-campus-topo/CAMPUS-FL2-C1-LEAF1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:35:00:00/' clab-rc-campus-topo/CAMPUS-FL2-C1-LEAF2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:36:00:00/' clab-rc-campus-topo/CAMPUS-FL2-C2-LEAF1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:37:00:00/' clab-rc-campus-topo/CAMPUS-FL2-C2-LEAF2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:38:00:00/' clab-rc-campus-topo/CAMPUS-FL3-C1-LEAF1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:39:00:00/' clab-rc-campus-topo/CAMPUS-FL3-C1-LEAF2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:40:00:00/' clab-rc-campus-topo/CAMPUS-FL3-C2-LEAF1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:41:00:00/' clab-rc-campus-topo/CAMPUS-FL3-C2-LEAF2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:42:00:00/' clab-rc-campus-topo/CAMPUS-WAN1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:43:00:00/' clab-rc-campus-topo/CAMPUS-WAN2/flash/system_mac_address
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-SPINE1/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-SPINE2/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL1-C1-SPLINE1/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL1-C1-SPLINE2/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL1-C2-SPLINE1/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL1-C2-SPLINE2/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL2-C1-SPLINE1/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL2-C1-SPLINE2/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL2-C2-SPLINE1/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL2-C2-SPLINE2/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL3-C1-SPLINE1/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL3-C1-SPLINE2/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL3-C2-SPLINE1/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL3-C2-SPLINE2/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL1-C1-LEAF1/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL1-C1-LEAF2/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL1-C2-LEAF1/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL1-C2-LEAF2/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL2-C1-LEAF1/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL2-C1-LEAF2/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL2-C2-LEAF1/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL2-C2-LEAF2/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL3-C1-LEAF1/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL3-C1-LEAF2/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL3-C2-LEAF1/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-FL3-C2-LEAF2/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-WAN1/flash/cv-onboarding-token
-sudo cp cv-onboarding-token clab-rc-campus-topo/CAMPUS-WAN2/flash/cv-onboarding-token
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:78:00:00/' clab-rc-campus-topo/CAMPUS-SPINE1/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:79:00:00/' clab-rc-campus-topo/CAMPUS-SPINE2/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:80:00:00/' clab-rc-campus-topo/CAMPUS-FL1-C1-SPLINE1/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:81:00:00/' clab-rc-campus-topo/CAMPUS-FL1-C1-SPLINE2/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:82:00:00/' clab-rc-campus-topo/CAMPUS-FL1-C2-SPLINE1/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:83:00:00/' clab-rc-campus-topo/CAMPUS-FL1-C2-SPLINE2/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:84:00:00/' clab-rc-campus-topo/CAMPUS-FL2-C1-SPLINE1/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:85:00:00/' clab-rc-campus-topo/CAMPUS-FL2-C1-SPLINE2/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:86:00:00/' clab-rc-campus-topo/CAMPUS-FL2-C2-SPLINE1/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:87:00:00/' clab-rc-campus-topo/CAMPUS-FL2-C2-SPLINE2/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:88:00:00/' clab-rc-campus-topo/CAMPUS-FL3-C1-SPLINE1/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:89:00:00/' clab-rc-campus-topo/CAMPUS-FL3-C1-SPLINE2/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:90:00:00/' clab-rc-campus-topo/CAMPUS-FL3-C2-SPLINE1/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:91:00:00/' clab-rc-campus-topo/CAMPUS-FL3-C2-SPLINE2/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:92:00:00/' clab-rc-campus-topo/CAMPUS-FL1-C1-LEAF1/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:93:00:00/' clab-rc-campus-topo/CAMPUS-FL1-C1-LEAF2/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:94:00:00/' clab-rc-campus-topo/CAMPUS-FL1-C2-LEAF1/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:95:00:00/' clab-rc-campus-topo/CAMPUS-FL1-C2-LEAF2/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:96:00:00/' clab-rc-campus-topo/CAMPUS-FL2-C1-LEAF1/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:97:00:00/' clab-rc-campus-topo/CAMPUS-FL2-C1-LEAF2/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:98:00:00/' clab-rc-campus-topo/CAMPUS-FL2-C2-LEAF1/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:99:00:00/' clab-rc-campus-topo/CAMPUS-FL2-C2-LEAF2/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:aa:00:00/' clab-rc-campus-topo/CAMPUS-FL3-C1-LEAF1/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:ab:00:00/' clab-rc-campus-topo/CAMPUS-FL3-C1-LEAF2/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:ac:00:00/' clab-rc-campus-topo/CAMPUS-FL3-C2-LEAF1/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:ad:00:00/' clab-rc-campus-topo/CAMPUS-FL3-C2-LEAF2/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:ae:00:00/' clab-rc-campus-topo/CAMPUS-WAN1/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:af:00:00/' clab-rc-campus-topo/CAMPUS-WAN2/flash/system_mac_address
