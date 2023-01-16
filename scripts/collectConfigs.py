7284f1bee44d79d8c09aa9ba8ab6651200a50602
Dimitri Capetz <dimitri.capetz@gmail.com>
2023-01-16 20:50:46 +0000

    basic update

--- scripts/collectConfigs.py
+++ scripts/collectConfigs.py
@@ -43,44 +43,44 @@ ips = ['10.255.63.195',
 for ip in ips:
     switch = Server("https://arista:arista@" + ip + "/command-api")
     # Getting Text version of Config and Show Command Outputs
-    configResponse = switch.runCmds(version=1, cmds=["enable",
-                                                     "show running-config", 
-                                                     "show isis segment-routing prefix-segments", 
-                                                     "show isis ti-lfa path detail"], format="text")
-    runningConfig = configResponse[1]['output']
-    prefixSids = configResponse[2]['output']
-    tilfaPath = configResponse[3]['output']
-    # Getting TI-LFA Path and PQ distance
-    isisResponse = switch.runCmds(1, ["enable",
-                                      "show isis ti-lfa path",
-                                      "show isis segment-routing prefix-segments"])
-    output = ""
-    maxNodeCount = 0
-    try:
-        for node in isisResponse[1]["vrfs"]["default"]["v4Info"]["topologies"]["2"]["destinations"].items():
-            destinationNode = node[1]["hostname"]
-            for path in node[1]["pathIds"].items():
-                if "excludeIntf" in path[1]["constraint"].keys():
-                    protection = "Link Protection"
-                else:
-                    protection = "Node Protection"
-                nodeCount = len(path[1]["sysIds"])
-                if nodeCount > maxNodeCount:
-                    maxNodeCount = nodeCount
-                if nodeCount > 4:
-                    output = output + "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n!POTENTIAL TI-LFA PATH ISSUE:!\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
-                nodeStr = str(len(path[1]["sysIds"])) + " Node(s)"
-                output = output + destinationNode + " - " + protection + " = " + nodeStr + "\n\n"
-        output = output + "\n\n!!!!!!!!!\nISIS Data\n!!!!!!!!!\n\n" + pprint.pformat(isisResponse[1]) + "\n\n" + pprint.pformat(isisResponse[2])
-    except KeyError:
-        output = "ISIS Disabled"
-    output = output + "\n\n!!!!!!\nCONFIG\n!!!!!!\n\n" + runningConfig + prefixSids + tilfaPath
-    hostname = switch.runCmds(1,["show hostname"])
-    if "ISIS Disabled" in output:
-        with open(hostname[0]['hostname'] + '-DISABLED.txt', 'w') as f:
-            f.write(output)
-    else:
-        with open(hostname[0]['hostname'] + '-' + str(maxNodeCount) + '.txt', 'w') as f:
-            f.write(output)
+    #configResponse = switch.runCmds(version=1, cmds=["enable",
+    #                                                 "show running-config", 
+    #                                                 "show isis segment-routing prefix-segments", 
+    #                                                 "show isis ti-lfa path detail"], format="text")
+    #runningConfig = configResponse[1]['output']
+    #prefixSids = configResponse[2]['output']
+    #tilfaPath = configResponse[3]['output']
+    ## Getting TI-LFA Path and PQ distance
+    #isisResponse = switch.runCmds(1, ["enable",
+    #                                  "show isis ti-lfa path",
+    #                                  "show isis segment-routing prefix-segments"])
+    #output = ""
+    #maxNodeCount = 0
+    #try:
+    #    for node in isisResponse[1]["vrfs"]["default"]["v4Info"]["topologies"]["2"]["destinations"].items():
+    #        destinationNode = node[1]["hostname"]
+    #        for path in node[1]["pathIds"].items():
+    #            if "excludeIntf" in path[1]["constraint"].keys():
+    #                protection = "Link Protection"
+    #            else:
+    #                protection = "Node Protection"
+    #            nodeCount = len(path[1]["sysIds"])
+    #            if nodeCount > maxNodeCount:
+    #                maxNodeCount = nodeCount
+    #            if nodeCount > 4:
+    #                output = output + "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n!POTENTIAL TI-LFA PATH ISSUE:!\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
+    #            nodeStr = str(len(path[1]["sysIds"])) + " Node(s)"
+    #            output = output + destinationNode + " - " + protection + " = " + nodeStr + "\n\n"
+    #    output = output + "\n\n!!!!!!!!!\nISIS Data\n!!!!!!!!!\n\n" + pprint.pformat(isisResponse[1]) + "\n\n" + pprint.pformat(isisResponse[2])
+    #except KeyError:
+    #    output = "ISIS Disabled"
+    #output = output + "\n\n!!!!!!\nCONFIG\n!!!!!!\n\n" + runningConfig + prefixSids + tilfaPath
+    #hostname = switch.runCmds(1,["show hostname"])
+    #if "ISIS Disabled" in output:
+    #    with open(hostname[0]['hostname'] + '-DISABLED.txt', 'w') as f:
+    #        f.write(output)
+    #else:
+    #    with open(hostname[0]['hostname'] + '-' + str(maxNodeCount) + '.txt', 'w') as f:
+    #        f.write(output)
     # Add Lines of Config to all devices
-    #response = switch.runCmds(1, ["enable", "configure", "router isis 100", "address-family ipv4 unicast", "fast-reroute ti-lfa srlg strict"])
+    response = switch.runCmds(1, ["enable", "configure", "router isis 100", "address-family ipv4 unicast", "maximum-paths 1"])
