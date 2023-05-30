b78fac5683bde7bd83f9060052caa6d0af02e07b
Dimitri Capetz <dimitri.capetz@gmail.com>
2023-05-30 15:01:30 +0000

    L2LS Lab

--- containerlab-topos/dc-topo/mac.sh
+++ containerlab-topos/dc-topo/mac.sh
@@ -10,13 +10,13 @@ sudo sed -i 's/..:..:..:..:..:../00:1c:73:18:00:00/' clab-dc-topo/DC-BORDER1/fla
 sudo sed -i 's/..:..:..:..:..:../00:1c:73:19:00:00/' clab-dc-topo/DC-BORDER2/flash/system_mac_address
 sudo sed -i 's/..:..:..:..:..:../00:1c:73:20:00:00/' clab-dc-topo/WAN1/flash/system_mac_address
 sudo sed -i 's/..:..:..:..:..:../00:1c:73:21:00:00/' clab-dc-topo/WAN2/flash/system_mac_address
-sudo cp ../cv-onboarding-token-stg clab-dc-topo/DC-SPINE1/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token-stg clab-dc-topo/DC-SPINE2/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token-stg clab-dc-topo/DC-LEAF1/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token-stg clab-dc-topo/DC-LEAF2/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token-stg clab-dc-topo/DC-LEAF3/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token-stg clab-dc-topo/DC-LEAF4/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token-stg clab-dc-topo/DC-LEAF5/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token-stg clab-dc-topo/DC-LEAF6/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token-stg clab-dc-topo/DC-BORDER1/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token-stg clab-dc-topo/DC-BORDER2/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dc-topo/DC-SPINE1/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dc-topo/DC-SPINE2/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dc-topo/DC-LEAF1/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dc-topo/DC-LEAF2/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dc-topo/DC-LEAF3/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dc-topo/DC-LEAF4/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dc-topo/DC-LEAF5/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dc-topo/DC-LEAF6/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dc-topo/DC-BORDER1/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dc-topo/DC-BORDER2/flash/cv-onboarding-token
