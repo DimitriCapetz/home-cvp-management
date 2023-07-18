4f1213ccbbe9520d61f62f79786d8ab25e0c993f
Dimitri Capetz <dimitri.capetz@gmail.com>
2023-07-18 15:36:49 +0000

    Re-baseline of Dual DC

--- containerlab-topos/dual-dc-topo/mac.sh
+++ containerlab-topos/dual-dc-topo/mac.sh
@@ -1,33 +1,135 @@
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:30:00:00/' clab-dual-dc-topo/ENT-DC1-SPINE1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:31:00:00/' clab-dual-dc-topo/ENT-DC1-SPINE2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:32:00:00/' clab-dual-dc-topo/ENT-DC1-LEAF1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:33:00:00/' clab-dual-dc-topo/ENT-DC1-LEAF2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:34:00:00/' clab-dual-dc-topo/ENT-DC1-LEAF3/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:35:00:00/' clab-dual-dc-topo/ENT-DC1-LEAF4/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:36:00:00/' clab-dual-dc-topo/ENT-DC1-LEAF5/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:37:00:00/' clab-dual-dc-topo/ENT-DC1-LEAF6/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:38:00:00/' clab-dual-dc-topo/ENT-DC1-BORDER1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:39:00:00/' clab-dual-dc-topo/ENT-DC1-BORDER2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:40:00:00/' clab-dual-dc-topo/ENT-DC1-HOST1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:41:00:00/' clab-dual-dc-topo/ENT-DC1-HOST2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:42:00:00/' clab-dual-dc-topo/ENT-DC1-HOST3/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:43:00:00/' clab-dual-dc-topo/ENT-DC1-CORE1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:44:00:00/' clab-dual-dc-topo/ENT-DC1-CORE2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:45:00:00/' clab-dual-dc-topo/ENT-DC2-SPINE1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:46:00:00/' clab-dual-dc-topo/ENT-DC2-SPINE2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:47:00:00/' clab-dual-dc-topo/ENT-DC2-LEAF1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:48:00:00/' clab-dual-dc-topo/ENT-DC2-LEAF2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:49:00:00/' clab-dual-dc-topo/ENT-DC2-LEAF3/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:50:00:00/' clab-dual-dc-topo/ENT-DC2-LEAF4/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:51:00:00/' clab-dual-dc-topo/ENT-DC2-LEAF5/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:52:00:00/' clab-dual-dc-topo/ENT-DC2-LEAF6/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:53:00:00/' clab-dual-dc-topo/ENT-DC2-BORDER1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:54:00:00/' clab-dual-dc-topo/ENT-DC2-BORDER2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:55:00:00/' clab-dual-dc-topo/ENT-DC2-HOST1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:56:00:00/' clab-dual-dc-topo/ENT-DC2-HOST2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:57:00:00/' clab-dual-dc-topo/ENT-DC2-HOST3/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:58:00:00/' clab-dual-dc-topo/ENT-DC2-CORE1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:59:00:00/' clab-dual-dc-topo/ENT-DC2-CORE2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:60:00:00/' clab-dual-dc-topo/ENT-DC1-WAN1/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:61:00:00/' clab-dual-dc-topo/ENT-DC2-WAN2/flash/system_mac_address
-sudo sed -i 's/..:..:..:..:..:../00:1c:73:62:00:00/' clab-dual-dc-topo/ENT-CORE-WAN3/flash/system_mac_address
+sudo touch clab-dual-dc-topo/ENT-DC1-SPINE1/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC1-SPINE2/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC1-LEAF1/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC1-LEAF2/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC1-LEAF3/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC1-LEAF4/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC1-LEAF5/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC1-LEAF6/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC1-BORDER1/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC1-BORDER2/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC1-CORE1/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC1-CORE2/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC2-SPINE1/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC2-SPINE2/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC2-LEAF1/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC2-LEAF2/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC2-LEAF3/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC2-LEAF4/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC2-LEAF5/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC2-LEAF6/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC2-BORDER1/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC2-BORDER2/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC2-CORE1/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC2-CORE2/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC1-WAN1/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-DC2-WAN2/flash/ceos-config
+sudo touch clab-dual-dc-topo/ENT-CORE-WAN3/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:30:00:00" >> clab-dual-dc-topo/ENT-DC1-SPINE1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:31:00:00" >> clab-dual-dc-topo/ENT-DC1-SPINE2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:32:00:00" >> clab-dual-dc-topo/ENT-DC1-LEAF1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:33:00:00" >> clab-dual-dc-topo/ENT-DC1-LEAF2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:34:00:00" >> clab-dual-dc-topo/ENT-DC1-LEAF3/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:35:00:00" >> clab-dual-dc-topo/ENT-DC1-LEAF4/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:36:00:00" >> clab-dual-dc-topo/ENT-DC1-LEAF5/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:37:00:00" >> clab-dual-dc-topo/ENT-DC1-LEAF6/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:38:00:00" >> clab-dual-dc-topo/ENT-DC1-BORDER1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:39:00:00" >> clab-dual-dc-topo/ENT-DC1-BORDER2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:43:00:00" >> clab-dual-dc-topo/ENT-DC1-CORE1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:44:00:00" >> clab-dual-dc-topo/ENT-DC1-CORE2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:45:00:00" >> clab-dual-dc-topo/ENT-DC2-SPINE1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:46:00:00" >> clab-dual-dc-topo/ENT-DC2-SPINE2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:47:00:00" >> clab-dual-dc-topo/ENT-DC2-LEAF1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:48:00:00" >> clab-dual-dc-topo/ENT-DC2-LEAF2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:49:00:00" >> clab-dual-dc-topo/ENT-DC2-LEAF3/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:50:00:00" >> clab-dual-dc-topo/ENT-DC2-LEAF4/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:51:00:00" >> clab-dual-dc-topo/ENT-DC2-LEAF5/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:52:00:00" >> clab-dual-dc-topo/ENT-DC2-LEAF6/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:53:00:00" >> clab-dual-dc-topo/ENT-DC2-BORDER1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:54:00:00" >> clab-dual-dc-topo/ENT-DC2-BORDER2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:58:00:00" >> clab-dual-dc-topo/ENT-DC2-CORE1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:59:00:00" >> clab-dual-dc-topo/ENT-DC2-CORE2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:60:00:00" >> clab-dual-dc-topo/ENT-DC1-WAN1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:61:00:00" >> clab-dual-dc-topo/ENT-DC2-WAN2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:dc:73:62:00:00" >> clab-dual-dc-topo/ENT-CORE-WAN3/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC1-SPINE1" >> clab-dual-dc-topo/ENT-DC1-SPINE1/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC1-SPINE2" >> clab-dual-dc-topo/ENT-DC1-SPINE2/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC1-LEAF1" >> clab-dual-dc-topo/ENT-DC1-LEAF1/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC1-LEAF2" >> clab-dual-dc-topo/ENT-DC1-LEAF2/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC1-LEAF3" >> clab-dual-dc-topo/ENT-DC1-LEAF3/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC1-LEAF4" >> clab-dual-dc-topo/ENT-DC1-LEAF4/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC1-LEAF5" >> clab-dual-dc-topo/ENT-DC1-LEAF5/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC1-LEAF6" >> clab-dual-dc-topo/ENT-DC1-LEAF6/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC1-BORDER1" >> clab-dual-dc-topo/ENT-DC1-BORDER1/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC1-BORDER2" >> clab-dual-dc-topo/ENT-DC1-BORDER2/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC1-CORE1" >> clab-dual-dc-topo/ENT-DC1-CORE1/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC1-CORE2" >> clab-dual-dc-topo/ENT-DC1-CORE2/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC2-SPINE1" >> clab-dual-dc-topo/ENT-DC2-SPINE1/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC2-SPINE2" >> clab-dual-dc-topo/ENT-DC2-SPINE2/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC2-LEAF1" >> clab-dual-dc-topo/ENT-DC2-LEAF1/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC2-LEAF2" >> clab-dual-dc-topo/ENT-DC2-LEAF2/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC2-LEAF3" >> clab-dual-dc-topo/ENT-DC2-LEAF3/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC2-LEAF4" >> clab-dual-dc-topo/ENT-DC2-LEAF4/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC2-LEAF5" >> clab-dual-dc-topo/ENT-DC2-LEAF5/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC2-LEAF6" >> clab-dual-dc-topo/ENT-DC2-LEAF6/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC2-BORDER1" >> clab-dual-dc-topo/ENT-DC2-BORDER1/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC2-BORDER2" >> clab-dual-dc-topo/ENT-DC2-BORDER2/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC2-CORE1" >> clab-dual-dc-topo/ENT-DC2-CORE1/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC2-CORE2" >> clab-dual-dc-topo/ENT-DC2-CORE2/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC1-WAN1" >> clab-dual-dc-topo/ENT-DC1-WAN1/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-DC2-WAN2" >> clab-dual-dc-topo/ENT-DC2-WAN2/flash/ceos-config
+sudo echo "SERIALNUMBER=ENT-CORE-WAN3" >> clab-dual-dc-topo/ENT-CORE-WAN3/flash/ceos-config
+sudo rm clab-dual-dc-topo/ENT-DC1-SPINE1/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC1-SPINE2/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC1-LEAF1/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC1-LEAF2/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC1-LEAF3/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC1-LEAF4/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC1-LEAF5/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC1-LEAF6/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC1-BORDER1/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC1-BORDER2/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC1-CORE1/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC1-CORE2/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC2-SPINE1/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC2-SPINE2/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC2-LEAF1/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC2-LEAF2/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC2-LEAF3/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC2-LEAF4/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC2-LEAF5/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC2-LEAF6/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC2-BORDER1/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC2-BORDER2/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC2-CORE1/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC2-CORE2/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC1-WAN1/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-DC2-WAN2/flash/system_mac_address
+sudo rm clab-dual-dc-topo/ENT-CORE-WAN3/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC1-SPINE1/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC1-SPINE2/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC1-LEAF1/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC1-LEAF2/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC1-LEAF3/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC1-LEAF4/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC1-LEAF5/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC1-LEAF6/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC1-BORDER1/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC1-BORDER2/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC1-CORE1/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC1-CORE2/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC2-SPINE1/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC2-SPINE2/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC2-LEAF1/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC2-LEAF2/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC2-LEAF3/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC2-LEAF4/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC2-LEAF5/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC2-LEAF6/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC2-BORDER1/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC2-BORDER2/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC2-CORE1/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC2-CORE2/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC1-WAN1/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-DC2-WAN2/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-dual-dc-topo/ENT-CORE-WAN3/flash/cv-onboarding-token
