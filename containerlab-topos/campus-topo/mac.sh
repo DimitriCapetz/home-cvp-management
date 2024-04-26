8c9b826a136236c8f683f610aed4635f9a749a35
Dimitri Capetz <dimitri.capetz@gmail.com>
2024-04-26 15:26:43 +0000

    typo

--- containerlab-topos/campus-topo/mac.sh
+++ containerlab-topos/campus-topo/mac.sh
@@ -1,75 +1,75 @@
-sudo touch campus-topo/CAMPUS-SPINE1/flash/ceos-config
-sudo touch campus-topo/CAMPUS-SPINE2/flash/ceos-config
-sudo touch campus-topo/CAMPUS-C1-LEAF1/flash/ceos-config
-sudo touch campus-topo/CAMPUS-C1-LEAF2/flash/ceos-config
-sudo touch campus-topo/CAMPUS-C2-LEAF1/flash/ceos-config
-sudo touch campus-topo/CAMPUS-C3-LEAF1/flash/ceos-config
-sudo touch campus-topo/CAMPUS-C3-LEAF2/flash/ceos-config
-sudo touch campus-topo/CAMPUS-C3-LEAF3/flash/ceos-config
-sudo touch campus-topo/CAMPUS-C3-LEAF4/flash/ceos-config
-sudo touch campus-topo/CAMPUS-C3-LEAF5/flash/ceos-config
-sudo touch campus-topo/CAMPUS-WAN1/flash/ceos-config
-sudo touch campus-topo/CAMPUS-WAN2/flash/ceos-config
-sudo touch campus-topo/CAMPUS-C1-HOST1/flash/ceos-config
-sudo touch campus-topo/CAMPUS-C2-HOST2/flash/ceos-config
-sudo touch campus-topo/CAMPUS-C3-AP1/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:00" >> campus-topo/CAMPUS-SPINE1/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:10" >> campus-topo/CAMPUS-SPINE2/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:20" >> campus-topo/CAMPUS-C1-LEAF1/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:30" >> campus-topo/CAMPUS-C1-LEAF2/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:40" >> campus-topo/CAMPUS-C2-LEAF1/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:50" >> campus-topo/CAMPUS-C3-LEAF1/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:60" >> campus-topo/CAMPUS-C3-LEAF2/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:70" >> campus-topo/CAMPUS-C3-LEAF3/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:80" >> campus-topo/CAMPUS-C3-LEAF4/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:90" >> campus-topo/CAMPUS-C3-LEAF5/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:a0" >> campus-topo/CAMPUS-WAN1/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:b0" >> campus-topo/CAMPUS-WAN2/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:c0" >> campus-topo/CAMPUS-C1-HOST1/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:d0" >> campus-topo/CAMPUS-C2-HOST2/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:e0" >> campus-topo/CAMPUS-C3-AP1/flash/ceos-config
-sudo echo "SERIALNUMBER=CAMPUS-SPINE1" >> campus-topo/CAMPUS-SPINE1/flash/ceos-config
-sudo echo "SERIALNUMBER=CAMPUS-SPINE2" >> campus-topo/CAMPUS-SPINE2/flash/ceos-config
-sudo echo "SERIALNUMBER=CAMPUS-C1-LEAF1" >> campus-topo/CAMPUS-C1-LEAF1/flash/ceos-config
-sudo echo "SERIALNUMBER=CAMPUS-C1-LEAF2" >> campus-topo/CAMPUS-C1-LEAF2/flash/ceos-config
-sudo echo "SERIALNUMBER=CAMPUS-C2-LEAF1" >> campus-topo/CAMPUS-C2-LEAF1/flash/ceos-config
-sudo echo "SERIALNUMBER=CAMPUS-C3-LEAF1" >> campus-topo/CAMPUS-C3-LEAF1/flash/ceos-config
-sudo echo "SERIALNUMBER=CAMPUS-C3-LEAF2" >> campus-topo/CAMPUS-C3-LEAF2/flash/ceos-config
-sudo echo "SERIALNUMBER=CAMPUS-C3-LEAF3" >> campus-topo/CAMPUS-C3-LEAF3/flash/ceos-config
-sudo echo "SERIALNUMBER=CAMPUS-C3-LEAF4" >> campus-topo/CAMPUS-C3-LEAF4/flash/ceos-config
-sudo echo "SERIALNUMBER=CAMPUS-C3-LEAF5" >> campus-topo/CAMPUS-C3-LEAF5/flash/ceos-config
-sudo echo "SERIALNUMBER=CAMPUS-WAN1" >> campus-topo/CAMPUS-WAN1/flash/ceos-config
-sudo echo "SERIALNUMBER=CAMPUS-WAN2" >> campus-topo/CAMPUS-WAN2/flash/ceos-config
-sudo echo "SERIALNUMBER=CAMPUS-C1-HOST1" >> campus-topo/CAMPUS-C1-HOST1/flash/ceos-config
-sudo echo "SERIALNUMBER=CAMPUS-C2-HOST2" >> campus-topo/CAMPUS-C2-HOST2/flash/ceos-config
-sudo echo "SERIALNUMBER=CAMPUS-C3-AP1" >> campus-topo/CAMPUS-C3-AP1/flash/ceos-config
-sudo rm campus-topo/CAMPUS-SPINE1/flash/system_mac_address
-sudo rm campus-topo/CAMPUS-SPINE2/flash/system_mac_address
-sudo rm campus-topo/CAMPUS-C1-LEAF1/flash/system_mac_address
-sudo rm campus-topo/CAMPUS-C1-LEAF2/flash/system_mac_address
-sudo rm campus-topo/CAMPUS-C2-LEAF1/flash/system_mac_address
-sudo rm campus-topo/CAMPUS-C3-LEAF1/flash/system_mac_address
-sudo rm campus-topo/CAMPUS-C3-LEAF2/flash/system_mac_address
-sudo rm campus-topo/CAMPUS-C3-LEAF3/flash/system_mac_address
-sudo rm campus-topo/CAMPUS-C3-LEAF4/flash/system_mac_address
-sudo rm campus-topo/CAMPUS-C3-LEAF5/flash/system_mac_address
-sudo rm campus-topo/CAMPUS-WAN1/flash/system_mac_address
-sudo rm campus-topo/CAMPUS-WAN2/flash/system_mac_address
-sudo rm campus-topo/CAMPUS-C1-HOST1/flash/system_mac_address
-sudo rm campus-topo/CAMPUS-C2-HOST2/flash/system_mac_address
-sudo rm campus-topo/CAMPUS-C3-AP1/flash/system_mac_address
-sudo cp ../cv-onboarding-token campus-topo/CAMPUS-SPINE1/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token campus-topo/CAMPUS-SPINE2/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token campus-topo/CAMPUS-C1-LEAF1/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token campus-topo/CAMPUS-C1-LEAF2/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token campus-topo/CAMPUS-C2-LEAF1/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token campus-topo/CAMPUS-C3-LEAF1/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token campus-topo/CAMPUS-C3-LEAF2/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token campus-topo/CAMPUS-C3-LEAF3/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token campus-topo/CAMPUS-C3-LEAF4/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token campus-topo/CAMPUS-C3-LEAF5/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token campus-topo/CAMPUS-WAN1/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token campus-topo/CAMPUS-WAN2/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token campus-topo/CAMPUS-C1-HOST1/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token campus-topo/CAMPUS-C2-HOST2/flash/cv-onboarding-token
-sudo cp ../cv-onboarding-token campus-topo/CAMPUS-C3-AP1/flash/cv-onboarding-token
+sudo touch clab-campus-topo/CAMPUS-SPINE1/flash/ceos-config
+sudo touch clab-campus-topo/CAMPUS-SPINE2/flash/ceos-config
+sudo touch clab-campus-topo/CAMPUS-C1-LEAF1/flash/ceos-config
+sudo touch clab-campus-topo/CAMPUS-C1-LEAF2/flash/ceos-config
+sudo touch clab-campus-topo/CAMPUS-C2-LEAF1/flash/ceos-config
+sudo touch clab-campus-topo/CAMPUS-C3-LEAF1/flash/ceos-config
+sudo touch clab-campus-topo/CAMPUS-C3-LEAF2/flash/ceos-config
+sudo touch clab-campus-topo/CAMPUS-C3-LEAF3/flash/ceos-config
+sudo touch clab-campus-topo/CAMPUS-C3-LEAF4/flash/ceos-config
+sudo touch clab-campus-topo/CAMPUS-C3-LEAF5/flash/ceos-config
+sudo touch clab-campus-topo/CAMPUS-WAN1/flash/ceos-config
+sudo touch clab-campus-topo/CAMPUS-WAN2/flash/ceos-config
+sudo touch clab-campus-topo/CAMPUS-C1-HOST1/flash/ceos-config
+sudo touch clab-campus-topo/CAMPUS-C2-HOST2/flash/ceos-config
+sudo touch clab-campus-topo/CAMPUS-C3-AP1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:00" >> clab-campus-topo/CAMPUS-SPINE1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:10" >> clab-campus-topo/CAMPUS-SPINE2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:20" >> clab-campus-topo/CAMPUS-C1-LEAF1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:30" >> clab-campus-topo/CAMPUS-C1-LEAF2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:40" >> clab-campus-topo/CAMPUS-C2-LEAF1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:50" >> clab-campus-topo/CAMPUS-C3-LEAF1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:60" >> clab-campus-topo/CAMPUS-C3-LEAF2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:70" >> clab-campus-topo/CAMPUS-C3-LEAF3/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:80" >> clab-campus-topo/CAMPUS-C3-LEAF4/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:90" >> clab-campus-topo/CAMPUS-C3-LEAF5/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:a0" >> clab-campus-topo/CAMPUS-WAN1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:b0" >> clab-campus-topo/CAMPUS-WAN2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:c0" >> clab-campus-topo/CAMPUS-C1-HOST1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:d0" >> clab-campus-topo/CAMPUS-C2-HOST2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:a0:00:e0" >> clab-campus-topo/CAMPUS-C3-AP1/flash/ceos-config
+sudo echo "SERIALNUMBER=CAMPUS-SPINE1" >> clab-campus-topo/CAMPUS-SPINE1/flash/ceos-config
+sudo echo "SERIALNUMBER=CAMPUS-SPINE2" >> clab-campus-topo/CAMPUS-SPINE2/flash/ceos-config
+sudo echo "SERIALNUMBER=CAMPUS-C1-LEAF1" >> clab-campus-topo/CAMPUS-C1-LEAF1/flash/ceos-config
+sudo echo "SERIALNUMBER=CAMPUS-C1-LEAF2" >> clab-campus-topo/CAMPUS-C1-LEAF2/flash/ceos-config
+sudo echo "SERIALNUMBER=CAMPUS-C2-LEAF1" >> clab-campus-topo/CAMPUS-C2-LEAF1/flash/ceos-config
+sudo echo "SERIALNUMBER=CAMPUS-C3-LEAF1" >> clab-campus-topo/CAMPUS-C3-LEAF1/flash/ceos-config
+sudo echo "SERIALNUMBER=CAMPUS-C3-LEAF2" >> clab-campus-topo/CAMPUS-C3-LEAF2/flash/ceos-config
+sudo echo "SERIALNUMBER=CAMPUS-C3-LEAF3" >> clab-campus-topo/CAMPUS-C3-LEAF3/flash/ceos-config
+sudo echo "SERIALNUMBER=CAMPUS-C3-LEAF4" >> clab-campus-topo/CAMPUS-C3-LEAF4/flash/ceos-config
+sudo echo "SERIALNUMBER=CAMPUS-C3-LEAF5" >> clab-campus-topo/CAMPUS-C3-LEAF5/flash/ceos-config
+sudo echo "SERIALNUMBER=CAMPUS-WAN1" >> clab-campus-topo/CAMPUS-WAN1/flash/ceos-config
+sudo echo "SERIALNUMBER=CAMPUS-WAN2" >> clab-campus-topo/CAMPUS-WAN2/flash/ceos-config
+sudo echo "SERIALNUMBER=CAMPUS-C1-HOST1" >> clab-campus-topo/CAMPUS-C1-HOST1/flash/ceos-config
+sudo echo "SERIALNUMBER=CAMPUS-C2-HOST2" >> clab-campus-topo/CAMPUS-C2-HOST2/flash/ceos-config
+sudo echo "SERIALNUMBER=CAMPUS-C3-AP1" >> clab-campus-topo/CAMPUS-C3-AP1/flash/ceos-config
+sudo rm clab-campus-topo/CAMPUS-SPINE1/flash/system_mac_address
+sudo rm clab-campus-topo/CAMPUS-SPINE2/flash/system_mac_address
+sudo rm clab-campus-topo/CAMPUS-C1-LEAF1/flash/system_mac_address
+sudo rm clab-campus-topo/CAMPUS-C1-LEAF2/flash/system_mac_address
+sudo rm clab-campus-topo/CAMPUS-C2-LEAF1/flash/system_mac_address
+sudo rm clab-campus-topo/CAMPUS-C3-LEAF1/flash/system_mac_address
+sudo rm clab-campus-topo/CAMPUS-C3-LEAF2/flash/system_mac_address
+sudo rm clab-campus-topo/CAMPUS-C3-LEAF3/flash/system_mac_address
+sudo rm clab-campus-topo/CAMPUS-C3-LEAF4/flash/system_mac_address
+sudo rm clab-campus-topo/CAMPUS-C3-LEAF5/flash/system_mac_address
+sudo rm clab-campus-topo/CAMPUS-WAN1/flash/system_mac_address
+sudo rm clab-campus-topo/CAMPUS-WAN2/flash/system_mac_address
+sudo rm clab-campus-topo/CAMPUS-C1-HOST1/flash/system_mac_address
+sudo rm clab-campus-topo/CAMPUS-C2-HOST2/flash/system_mac_address
+sudo rm clab-campus-topo/CAMPUS-C3-AP1/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-campus-topo/CAMPUS-SPINE1/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-campus-topo/CAMPUS-SPINE2/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-campus-topo/CAMPUS-C1-LEAF1/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-campus-topo/CAMPUS-C1-LEAF2/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-campus-topo/CAMPUS-C2-LEAF1/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-campus-topo/CAMPUS-C3-LEAF1/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-campus-topo/CAMPUS-C3-LEAF2/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-campus-topo/CAMPUS-C3-LEAF3/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-campus-topo/CAMPUS-C3-LEAF4/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-campus-topo/CAMPUS-C3-LEAF5/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-campus-topo/CAMPUS-WAN1/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-campus-topo/CAMPUS-WAN2/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-campus-topo/CAMPUS-C1-HOST1/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-campus-topo/CAMPUS-C2-HOST2/flash/cv-onboarding-token
+sudo cp ../cv-onboarding-token clab-campus-topo/CAMPUS-C3-AP1/flash/cv-onboarding-token
