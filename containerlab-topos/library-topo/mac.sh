5a8d2ebf81d2e19745cd0587c1d02eef3598c20f
Dimitri Capetz <dimitri.capetz@gmail.com>
2024-06-10 19:30:18 +0000

    Library Topo

---
+++ containerlab-topos/library-topo/mac.sh
@@ -0,0 +1,30 @@
+sudo touch clab-library-topo/LIBRARY-LEAF1-A/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:00" >> clab-library-topo/LIBRARY-LEAF1-A/flash/ceos-config
+sudo echo "SERIALNUMBER=LIBRARY-LEAF1-A" >> clab-library-topo/LIBRARY-LEAF1-A/flash/ceos-config
+sudo rm clab-library-topo/LIBRARY-LEAF1-A/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-library-topo/LIBRARY-LEAF1-A/flash/cv-onboarding-token
+sudo touch clab-library-topo/LIBRARY-LEAF1-B/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:01" >> clab-library-topo/LIBRARY-LEAF1-B/flash/ceos-config
+sudo echo "SERIALNUMBER=LIBRARY-LEAF1-B" >> clab-library-topo/LIBRARY-LEAF1-B/flash/ceos-config
+sudo rm clab-library-topo/LIBRARY-LEAF1-B/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-library-topo/LIBRARY-LEAF1-B/flash/cv-onboarding-token
+sudo touch clab-library-topo/LIBRARY-LEAF1-C/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:02" >> clab-library-topo/LIBRARY-LEAF1-C/flash/ceos-config
+sudo echo "SERIALNUMBER=LIBRARY-LEAF1-C" >> clab-library-topo/LIBRARY-LEAF1-C/flash/ceos-config
+sudo rm clab-library-topo/LIBRARY-LEAF1-C/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-library-topo/LIBRARY-LEAF1-C/flash/cv-onboarding-token
+sudo touch clab-library-topo/LIBRARY-LEAF1-D/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:03" >> clab-library-topo/LIBRARY-LEAF1-D/flash/ceos-config
+sudo echo "SERIALNUMBER=LIBRARY-LEAF1-D" >> clab-library-topo/LIBRARY-LEAF1-D/flash/ceos-config
+sudo rm clab-library-topo/LIBRARY-LEAF1-D/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-library-topo/LIBRARY-LEAF1-D/flash/cv-onboarding-token
+sudo touch clab-library-topo/LIBRARY-HEAD-END1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:04" >> clab-library-topo/LIBRARY-HEAD-END1/flash/ceos-config
+sudo echo "SERIALNUMBER=LIBRARY-HEAD-END1" >> clab-library-topo/LIBRARY-HEAD-END1/flash/ceos-config
+sudo rm clab-library-topo/LIBRARY-HEAD-END1/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-library-topo/LIBRARY-HEAD-END1/flash/cv-onboarding-token
+sudo touch clab-library-topo/LIBRARY-HEAD-END2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:05" >> clab-library-topo/LIBRARY-HEAD-END2/flash/ceos-config
+sudo echo "SERIALNUMBER=LIBRARY-HEAD-END2" >> clab-library-topo/LIBRARY-HEAD-END2/flash/ceos-config
+sudo rm clab-library-topo/LIBRARY-HEAD-END2/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-library-topo/LIBRARY-HEAD-END2/flash/cv-onboarding-token
