9294cd7bf00d6e8a0a9fc6015743f5d3b8a43079
Dimitri Capetz <dimitri.capetz@gmail.com>
2024-06-10 17:25:49 +0000

    New topo gen

---
+++ containerlab-topos/simple-dci-topo/mac.sh
@@ -0,0 +1,41 @@
+sudo touch simple-dci-topo/DC1-SPINE1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:00" >> simple-dci-topo/DC1-SPINE1/flash/ceos-config
+sudo echo "SERIALNUMBER=DC1-SPINE1" >> simple-dci-topo/DC1-SPINE1/flash/ceos-config
+sudo rm simple-dci-topo/DC1-SPINE1/flash/system_mac_address
+sudo cp ../cv-onboarding-token simple-dci-topo/DC1-SPINE1/flash/cv-onboarding-tokensudo touch simple-dci-topo/DC1-SPINE2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:01" >> simple-dci-topo/DC1-SPINE2/flash/ceos-config
+sudo echo "SERIALNUMBER=DC1-SPINE2" >> simple-dci-topo/DC1-SPINE2/flash/ceos-config
+sudo rm simple-dci-topo/DC1-SPINE2/flash/system_mac_address
+sudo cp ../cv-onboarding-token simple-dci-topo/DC1-SPINE2/flash/cv-onboarding-tokensudo touch simple-dci-topo/DC2-SPINE1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:02" >> simple-dci-topo/DC2-SPINE1/flash/ceos-config
+sudo echo "SERIALNUMBER=DC2-SPINE1" >> simple-dci-topo/DC2-SPINE1/flash/ceos-config
+sudo rm simple-dci-topo/DC2-SPINE1/flash/system_mac_address
+sudo cp ../cv-onboarding-token simple-dci-topo/DC2-SPINE1/flash/cv-onboarding-tokensudo touch simple-dci-topo/DC2-SPINE2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:03" >> simple-dci-topo/DC2-SPINE2/flash/ceos-config
+sudo echo "SERIALNUMBER=DC2-SPINE2" >> simple-dci-topo/DC2-SPINE2/flash/ceos-config
+sudo rm simple-dci-topo/DC2-SPINE2/flash/system_mac_address
+sudo cp ../cv-onboarding-token simple-dci-topo/DC2-SPINE2/flash/cv-onboarding-tokensudo touch simple-dci-topo/DC1-LEAF1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:04" >> simple-dci-topo/DC1-LEAF1/flash/ceos-config
+sudo echo "SERIALNUMBER=DC1-LEAF1" >> simple-dci-topo/DC1-LEAF1/flash/ceos-config
+sudo rm simple-dci-topo/DC1-LEAF1/flash/system_mac_address
+sudo cp ../cv-onboarding-token simple-dci-topo/DC1-LEAF1/flash/cv-onboarding-tokensudo touch simple-dci-topo/DC1-BORDER1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:05" >> simple-dci-topo/DC1-BORDER1/flash/ceos-config
+sudo echo "SERIALNUMBER=DC1-BORDER1" >> simple-dci-topo/DC1-BORDER1/flash/ceos-config
+sudo rm simple-dci-topo/DC1-BORDER1/flash/system_mac_address
+sudo cp ../cv-onboarding-token simple-dci-topo/DC1-BORDER1/flash/cv-onboarding-tokensudo touch simple-dci-topo/DC1-BORDER2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:06" >> simple-dci-topo/DC1-BORDER2/flash/ceos-config
+sudo echo "SERIALNUMBER=DC1-BORDER2" >> simple-dci-topo/DC1-BORDER2/flash/ceos-config
+sudo rm simple-dci-topo/DC1-BORDER2/flash/system_mac_address
+sudo cp ../cv-onboarding-token simple-dci-topo/DC1-BORDER2/flash/cv-onboarding-tokensudo touch simple-dci-topo/DC2-LEAF1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:07" >> simple-dci-topo/DC2-LEAF1/flash/ceos-config
+sudo echo "SERIALNUMBER=DC2-LEAF1" >> simple-dci-topo/DC2-LEAF1/flash/ceos-config
+sudo rm simple-dci-topo/DC2-LEAF1/flash/system_mac_address
+sudo cp ../cv-onboarding-token simple-dci-topo/DC2-LEAF1/flash/cv-onboarding-tokensudo touch simple-dci-topo/DC2-BORDER1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:08" >> simple-dci-topo/DC2-BORDER1/flash/ceos-config
+sudo echo "SERIALNUMBER=DC2-BORDER1" >> simple-dci-topo/DC2-BORDER1/flash/ceos-config
+sudo rm simple-dci-topo/DC2-BORDER1/flash/system_mac_address
+sudo cp ../cv-onboarding-token simple-dci-topo/DC2-BORDER1/flash/cv-onboarding-tokensudo touch simple-dci-topo/DC2-BORDER2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:09" >> simple-dci-topo/DC2-BORDER2/flash/ceos-config
+sudo echo "SERIALNUMBER=DC2-BORDER2" >> simple-dci-topo/DC2-BORDER2/flash/ceos-config
+sudo rm simple-dci-topo/DC2-BORDER2/flash/system_mac_address
+sudo cp ../cv-onboarding-token simple-dci-topo/DC2-BORDER2/flash/cv-onboarding-token
