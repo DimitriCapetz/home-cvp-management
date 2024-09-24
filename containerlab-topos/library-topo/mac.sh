0a7d6ee5344a49dfa8ccbfc5514ef30d62d5972d
Dimitri Capetz <dimitri.capetz@gmail.com>
2024-09-24 18:19:27 +0000

    Updating to fix naming

--- containerlab-topos/library-topo/mac.sh
+++ containerlab-topos/library-topo/mac.sh
@@ -1,23 +1,23 @@
-sudo touch clab-library-topo/LIBRARY-LEAF1-A/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:00" >> clab-library-topo/LIBRARY-LEAF1-A/flash/ceos-config
-sudo echo "SERIALNUMBER=LIBRARY-LEAF1-A" >> clab-library-topo/LIBRARY-LEAF1-A/flash/ceos-config
-sudo rm clab-library-topo/LIBRARY-LEAF1-A/flash/system_mac_address
-sudo cp ../cv-onboarding-token clab-library-topo/LIBRARY-LEAF1-A/flash/cv-onboarding-token
-sudo touch clab-library-topo/LIBRARY-LEAF1-B/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:01" >> clab-library-topo/LIBRARY-LEAF1-B/flash/ceos-config
-sudo echo "SERIALNUMBER=LIBRARY-LEAF1-B" >> clab-library-topo/LIBRARY-LEAF1-B/flash/ceos-config
-sudo rm clab-library-topo/LIBRARY-LEAF1-B/flash/system_mac_address
-sudo cp ../cv-onboarding-token clab-library-topo/LIBRARY-LEAF1-B/flash/cv-onboarding-token
-sudo touch clab-library-topo/LIBRARY-LEAF1-C/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:02" >> clab-library-topo/LIBRARY-LEAF1-C/flash/ceos-config
-sudo echo "SERIALNUMBER=LIBRARY-LEAF1-C" >> clab-library-topo/LIBRARY-LEAF1-C/flash/ceos-config
-sudo rm clab-library-topo/LIBRARY-LEAF1-C/flash/system_mac_address
-sudo cp ../cv-onboarding-token clab-library-topo/LIBRARY-LEAF1-C/flash/cv-onboarding-token
-sudo touch clab-library-topo/LIBRARY-LEAF1-D/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:03" >> clab-library-topo/LIBRARY-LEAF1-D/flash/ceos-config
-sudo echo "SERIALNUMBER=LIBRARY-LEAF1-D" >> clab-library-topo/LIBRARY-LEAF1-D/flash/ceos-config
-sudo rm clab-library-topo/LIBRARY-LEAF1-D/flash/system_mac_address
-sudo cp ../cv-onboarding-token clab-library-topo/LIBRARY-LEAF1-D/flash/cv-onboarding-token
+sudo touch clab-library-topo/085-LL5-POC-EA1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:00" >> clab-library-topo/085-LL5-POC-EA1/flash/ceos-config
+sudo echo "SERIALNUMBER=085-LL5-POC-EA1" >> clab-library-topo/085-LL5-POC-EA1/flash/ceos-config
+sudo rm clab-library-topo/085-LL5-POC-EA1/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-library-topo/085-LL5-POC-EA1/flash/cv-onboarding-token
+sudo touch clab-library-topo/085-LL5-POC-EA2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:01" >> clab-library-topo/085-LL5-POC-EA2/flash/ceos-config
+sudo echo "SERIALNUMBER=085-LL5-POC-EA2" >> clab-library-topo/085-LL5-POC-EA2/flash/ceos-config
+sudo rm clab-library-topo/085-LL5-POC-EA2/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-library-topo/085-LL5-POC-EA2/flash/cv-onboarding-token
+sudo touch clab-library-topo/085-LL5-POC-EA3/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:02" >> clab-library-topo/085-LL5-POC-EA3/flash/ceos-config
+sudo echo "SERIALNUMBER=085-LL5-POC-EA3" >> clab-library-topo/085-LL5-POC-EA3/flash/ceos-config
+sudo rm clab-library-topo/085-LL5-POC-EA3/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-library-topo/085-LL5-POC-EA3/flash/cv-onboarding-token
+sudo touch clab-library-topo/085-LL5-POC-EA4/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:03" >> clab-library-topo/085-LL5-POC-EA4/flash/ceos-config
+sudo echo "SERIALNUMBER=085-LL5-POC-EA4" >> clab-library-topo/085-LL5-POC-EA4/flash/ceos-config
+sudo rm clab-library-topo/085-LL5-POC-EA4/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-library-topo/085-LL5-POC-EA4/flash/cv-onboarding-token
 sudo touch clab-library-topo/LIBRARY-HEAD-END1/flash/ceos-config
 sudo echo "SYSTEMMACADDR=00:1c:73:dc:00:04" >> clab-library-topo/LIBRARY-HEAD-END1/flash/ceos-config
 sudo echo "SERIALNUMBER=LIBRARY-HEAD-END1" >> clab-library-topo/LIBRARY-HEAD-END1/flash/ceos-config
