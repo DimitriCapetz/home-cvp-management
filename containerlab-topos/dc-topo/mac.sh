dabd3cf570c2960ce64b4c8b98b228a3bed33e77
Dimitri Capetz <dimitri.capetz@gmail.com>
2024-11-19 01:23:15 +0000

    File edits

--- containerlab-topos/dc-topo/mac.sh
+++ containerlab-topos/dc-topo/mac.sh
@@ -1,60 +1,60 @@
-sudo touch clab-dc-topo/dcapetz-DC1-SPINE1/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:01" >> clab-dc-topo/dcapetz-DC1-SPINE1/flash/ceos-config
-sudo echo "SERIALNUMBER=dcapetz-DC1-SPINE1" >> clab-dc-topo/dcapetz-DC1-SPINE1/flash/ceos-config
-sudo rm clab-dc-topo/dcapetz-DC1-SPINE1/flash/system_mac_address
-sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC1-SPINE1/flash/cv-onboarding-token
-sudo touch clab-dc-topo/dcapetz-DC1-SPINE2/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:02" >> clab-dc-topo/dcapetz-DC1-SPINE2/flash/ceos-config
-sudo echo "SERIALNUMBER=dcapetz-DC1-SPINE2" >> clab-dc-topo/dcapetz-DC1-SPINE2/flash/ceos-config
-sudo rm clab-dc-topo/dcapetz-DC1-SPINE2/flash/system_mac_address
-sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC1-SPINE2/flash/cv-onboarding-token
-sudo touch clab-dc-topo/dcapetz-DC1-LEAF1/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:11" >> clab-dc-topo/dcapetz-DC1-LEAF1/flash/ceos-config
-sudo echo "SERIALNUMBER=dcapetz-DC1-LEAF1" >> clab-dc-topo/dcapetz-DC1-LEAF1/flash/ceos-config
-sudo rm clab-dc-topo/dcapetz-DC1-LEAF1/flash/system_mac_address
-sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC1-LEAF1/flash/cv-onboarding-token
-sudo touch clab-dc-topo/dcapetz-DC1-LEAF2/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:12" >> clab-dc-topo/dcapetz-DC1-LEAF2/flash/ceos-config
-sudo echo "SERIALNUMBER=dcapetz-DC1-LEAF2" >> clab-dc-topo/dcapetz-DC1-LEAF2/flash/ceos-config
-sudo rm clab-dc-topo/dcapetz-DC1-LEAF2/flash/system_mac_address
-sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC1-LEAF2/flash/cv-onboarding-token
-sudo touch clab-dc-topo/dcapetz-DC1-LEAF3/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:13" >> clab-dc-topo/dcapetz-DC1-LEAF3/flash/ceos-config
-sudo echo "SERIALNUMBER=dcapetz-DC1-LEAF3" >> clab-dc-topo/dcapetz-DC1-LEAF3/flash/ceos-config
-sudo rm clab-dc-topo/dcapetz-DC1-LEAF3/flash/system_mac_address
-sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC1-LEAF3/flash/cv-onboarding-token
-sudo touch clab-dc-topo/dcapetz-DC1-LEAF4/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:14" >> clab-dc-topo/dcapetz-DC1-LEAF4/flash/ceos-config
-sudo echo "SERIALNUMBER=dcapetz-DC1-LEAF4" >> clab-dc-topo/dcapetz-DC1-LEAF4/flash/ceos-config
-sudo rm clab-dc-topo/dcapetz-DC1-LEAF4/flash/system_mac_address
-sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC1-LEAF4/flash/cv-onboarding-token
-sudo touch clab-dc-topo/dcapetz-DC1-LEAF5/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:15" >> clab-dc-topo/dcapetz-DC1-LEAF5/flash/ceos-config
-sudo echo "SERIALNUMBER=dcapetz-DC1-LEAF5" >> clab-dc-topo/dcapetz-DC1-LEAF5/flash/ceos-config
-sudo rm clab-dc-topo/dcapetz-DC1-LEAF5/flash/system_mac_address
-sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC1-LEAF5/flash/cv-onboarding-token
-sudo touch clab-dc-topo/dcapetz-DC1-LEAF6/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:16" >> clab-dc-topo/dcapetz-DC1-LEAF6/flash/ceos-config
-sudo echo "SERIALNUMBER=dcapetz-DC1-LEAF6" >> clab-dc-topo/dcapetz-DC1-LEAF6/flash/ceos-config
-sudo rm clab-dc-topo/dcapetz-DC1-LEAF6/flash/system_mac_address
-sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC1-LEAF6/flash/cv-onboarding-token
-sudo touch clab-dc-topo/dcapetz-DC1-BORDER1/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:21" >> clab-dc-topo/dcapetz-DC1-BORDER1/flash/ceos-config
-sudo echo "SERIALNUMBER=dcapetz-DC1-BORDER1" >> clab-dc-topo/dcapetz-DC1-BORDER1/flash/ceos-config
-sudo rm clab-dc-topo/dcapetz-DC1-BORDER1/flash/system_mac_address
-sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC1-BORDER1/flash/cv-onboarding-token
-sudo touch clab-dc-topo/dcapetz-DC1-BORDER2/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:22" >> clab-dc-topo/dcapetz-DC1-BORDER2/flash/ceos-config
-sudo echo "SERIALNUMBER=dcapetz-DC1-BORDER2" >> clab-dc-topo/dcapetz-DC1-BORDER2/flash/ceos-config
-sudo rm clab-dc-topo/dcapetz-DC1-BORDER2/flash/system_mac_address
-sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC1-BORDER2/flash/cv-onboarding-token
-sudo touch clab-dc-topo/dcapetz-DC1-WAN1/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:31" >> clab-dc-topo/dcapetz-DC1-WAN1/flash/ceos-config
-sudo echo "SERIALNUMBER=dcapetz-DC1-WAN1" >> clab-dc-topo/dcapetz-DC1-WAN1/flash/ceos-config
-sudo rm clab-dc-topo/dcapetz-DC1-WAN1/flash/system_mac_address
-sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC1-WAN1/flash/cv-onboarding-token
-sudo touch clab-dc-topo/dcapetz-DC1-WAN2/flash/ceos-config
-sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:32" >> clab-dc-topo/dcapetz-DC1-WAN2/flash/ceos-config
-sudo echo "SERIALNUMBER=dcapetz-DC1-WAN2" >> clab-dc-topo/dcapetz-DC1-WAN2/flash/ceos-config
-sudo rm clab-dc-topo/dcapetz-DC1-WAN2/flash/system_mac_address
-sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC1-WAN2/flash/cv-onboarding-token
+sudo touch clab-dc-topo/dcapetz-DC-SPINE1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:01" >> clab-dc-topo/dcapetz-DC-SPINE1/flash/ceos-config
+sudo echo "SERIALNUMBER=dcapetz-DC-SPINE1" >> clab-dc-topo/dcapetz-DC-SPINE1/flash/ceos-config
+sudo rm clab-dc-topo/dcapetz-DC-SPINE1/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC-SPINE1/flash/cv-onboarding-token
+sudo touch clab-dc-topo/dcapetz-DC-SPINE2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:02" >> clab-dc-topo/dcapetz-DC-SPINE2/flash/ceos-config
+sudo echo "SERIALNUMBER=dcapetz-DC-SPINE2" >> clab-dc-topo/dcapetz-DC-SPINE2/flash/ceos-config
+sudo rm clab-dc-topo/dcapetz-DC-SPINE2/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC-SPINE2/flash/cv-onboarding-token
+sudo touch clab-dc-topo/dcapetz-DC-LEAF1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:11" >> clab-dc-topo/dcapetz-DC-LEAF1/flash/ceos-config
+sudo echo "SERIALNUMBER=dcapetz-DC-LEAF1" >> clab-dc-topo/dcapetz-DC-LEAF1/flash/ceos-config
+sudo rm clab-dc-topo/dcapetz-DC-LEAF1/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC-LEAF1/flash/cv-onboarding-token
+sudo touch clab-dc-topo/dcapetz-DC-LEAF2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:12" >> clab-dc-topo/dcapetz-DC-LEAF2/flash/ceos-config
+sudo echo "SERIALNUMBER=dcapetz-DC-LEAF2" >> clab-dc-topo/dcapetz-DC-LEAF2/flash/ceos-config
+sudo rm clab-dc-topo/dcapetz-DC-LEAF2/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC-LEAF2/flash/cv-onboarding-token
+sudo touch clab-dc-topo/dcapetz-DC-LEAF3/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:13" >> clab-dc-topo/dcapetz-DC-LEAF3/flash/ceos-config
+sudo echo "SERIALNUMBER=dcapetz-DC-LEAF3" >> clab-dc-topo/dcapetz-DC-LEAF3/flash/ceos-config
+sudo rm clab-dc-topo/dcapetz-DC-LEAF3/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC-LEAF3/flash/cv-onboarding-token
+sudo touch clab-dc-topo/dcapetz-DC-LEAF4/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:14" >> clab-dc-topo/dcapetz-DC-LEAF4/flash/ceos-config
+sudo echo "SERIALNUMBER=dcapetz-DC-LEAF4" >> clab-dc-topo/dcapetz-DC-LEAF4/flash/ceos-config
+sudo rm clab-dc-topo/dcapetz-DC-LEAF4/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC-LEAF4/flash/cv-onboarding-token
+sudo touch clab-dc-topo/dcapetz-DC-LEAF5/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:15" >> clab-dc-topo/dcapetz-DC-LEAF5/flash/ceos-config
+sudo echo "SERIALNUMBER=dcapetz-DC-LEAF5" >> clab-dc-topo/dcapetz-DC-LEAF5/flash/ceos-config
+sudo rm clab-dc-topo/dcapetz-DC-LEAF5/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC-LEAF5/flash/cv-onboarding-token
+sudo touch clab-dc-topo/dcapetz-DC-LEAF6/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:16" >> clab-dc-topo/dcapetz-DC-LEAF6/flash/ceos-config
+sudo echo "SERIALNUMBER=dcapetz-DC-LEAF6" >> clab-dc-topo/dcapetz-DC-LEAF6/flash/ceos-config
+sudo rm clab-dc-topo/dcapetz-DC-LEAF6/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC-LEAF6/flash/cv-onboarding-token
+sudo touch clab-dc-topo/dcapetz-DC-BORDER1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:21" >> clab-dc-topo/dcapetz-DC-BORDER1/flash/ceos-config
+sudo echo "SERIALNUMBER=dcapetz-DC-BORDER1" >> clab-dc-topo/dcapetz-DC-BORDER1/flash/ceos-config
+sudo rm clab-dc-topo/dcapetz-DC-BORDER1/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC-BORDER1/flash/cv-onboarding-token
+sudo touch clab-dc-topo/dcapetz-DC-BORDER2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:22" >> clab-dc-topo/dcapetz-DC-BORDER2/flash/ceos-config
+sudo echo "SERIALNUMBER=dcapetz-DC-BORDER2" >> clab-dc-topo/dcapetz-DC-BORDER2/flash/ceos-config
+sudo rm clab-dc-topo/dcapetz-DC-BORDER2/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC-BORDER2/flash/cv-onboarding-token
+sudo touch clab-dc-topo/dcapetz-DC-WAN1/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:31" >> clab-dc-topo/dcapetz-DC-WAN1/flash/ceos-config
+sudo echo "SERIALNUMBER=dcapetz-DC-WAN1" >> clab-dc-topo/dcapetz-DC-WAN1/flash/ceos-config
+sudo rm clab-dc-topo/dcapetz-DC-WAN1/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC-WAN1/flash/cv-onboarding-token
+sudo touch clab-dc-topo/dcapetz-DC-WAN2/flash/ceos-config
+sudo echo "SYSTEMMACADDR=00:1c:73:dc:10:32" >> clab-dc-topo/dcapetz-DC-WAN2/flash/ceos-config
+sudo echo "SERIALNUMBER=dcapetz-DC-WAN2" >> clab-dc-topo/dcapetz-DC-WAN2/flash/ceos-config
+sudo rm clab-dc-topo/dcapetz-DC-WAN2/flash/system_mac_address
+sudo cp ../cv-onboarding-token clab-dc-topo/dcapetz-DC-WAN2/flash/cv-onboarding-token
